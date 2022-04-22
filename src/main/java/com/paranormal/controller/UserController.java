1d05345bd3d2752f8bce88efc6a16d3ef847dd55
tahafzl123 <taha.1129@outlook.com>
2022-04-23 01:25:59 +0300

    .

--- src/main/java/com/paranormal/controller/UserController.java
+++ src/main/java/com/paranormal/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.paranormal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +29,11 @@ public class UserController {
 	@PostMapping("/login")
 	public String login(@RequestBody LoginRequest request) {
 		return this.userService.login(request);
+	}
+	
+	@GetMapping("/all")
+	public List<User> findAll(){
+		return this.userService.findAll();
 	}
 	
 
