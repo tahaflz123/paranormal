cb6aec547dea5057cd1bb6b906caa5f726bcce54
tahafzl123 <taha.1129@outlook.com>
2022-04-23 01:24:23 +0300

    some modifications

--- src/main/java/com/paranormal/entity/post/Post.java
+++ src/main/java/com/paranormal/entity/post/Post.java
@@ -1,25 +1,38 @@
 package com.paranormal.entity.post;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.paranormal.entity.BaseEntity;
+import com.paranormal.entity.comment.Comment;
 import com.paranormal.entity.user.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "post")
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Builder
 public class Post extends BaseEntity{
 
@@ -29,7 +42,11 @@ public class Post extends BaseEntity{
 	@Lob
 	private String content;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
+	
+	@OneToMany(mappedBy = "post",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	public List<Comment> comment;
+	
 }
--- src/main/java/com/paranormal/entity/user/User.java
+++ src/main/java/com/paranormal/entity/user/User.java
@@ -1,28 +1,35 @@
 package com.paranormal.entity.user;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.paranormal.entity.BaseEntity;
 import com.paranormal.entity.post.Post;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "user")
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Builder
-public class User extends BaseEntity{
+public class User extends BaseEntity {
 	
 	@Column(name = "username", unique = true)
 	private String username;
@@ -32,7 +39,7 @@ public class User extends BaseEntity{
 	
 	private String password;
 	
-	@OneToMany(mappedBy = "user")
-	private List<Post> post;
+	@OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+	private List<Post> posts;
 
 }
