79573223e70f0739e430fe08394b68824b9b1eb2
tahafzl123 <taha.1129@outlook.com>
2022-04-23 01:24:01 +0300

    some modifications

--- src/main/java/com/paranormal/entity/comment/Comment.java
+++ src/main/java/com/paranormal/entity/comment/Comment.java
@@ -1,11 +1,18 @@
 package com.paranormal.entity.comment;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.paranormal.entity.BaseEntity;
 import com.paranormal.entity.post.Post;
 import com.paranormal.entity.user.User;
@@ -15,6 +22,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "comment")
@@ -23,18 +31,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@ToString
 public class Comment extends BaseEntity{
 
 	@Lob
 	private String content;
 	
-	@ManyToOne
-	@JoinColumn(name = "post_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
 	private User sender;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id")
 	private Post post;
-	
 	
 }
