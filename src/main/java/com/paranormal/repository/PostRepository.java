9e0f42f1aca5f0941054357c7e6a67d122badefe
tahafzl123 <taha.1129@outlook.com>
2022-04-23 03:24:27 +0300

    added findAllByHeader query

--- src/main/java/com/paranormal/repository/PostRepository.java
+++ src/main/java/com/paranormal/repository/PostRepository.java
@@ -1,6 +1,9 @@
 package com.paranormal.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.paranormal.entity.post.Post;
@@ -9,5 +12,8 @@ import com.paranormal.entity.post.Post;
 public interface PostRepository extends JpaRepository<Post, Long>{
 
 	boolean existsByHeader(String header);
+
+	@Query("SELECT p FROM Post p WHERE p.header like lower(concat('%', :q, '%'))")
+	Page<Post> findAllByHeaderLike(String q, Pageable pageable);
 	
 }
