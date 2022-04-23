55f755aed425acb0076f4596979b571dc52ec099
tahafzl123 <taha.1129@outlook.com>
2022-04-23 03:23:55 +0300

    search method created

--- src/main/java/com/paranormal/controller/SearchController.java
+++ src/main/java/com/paranormal/controller/SearchController.java
@@ -1,5 +1,25 @@
 package com.paranormal.controller;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paranormal.dto.response.SearchResponse;
+import com.paranormal.service.SearchService;
+
+@RestController
+@RequestMapping("/api/search")
 public class SearchController {
 
+	@Autowired
+	private SearchService searchService;
+	
+	@GetMapping
+	public SearchResponse search(@PathParam("q") String q) {
+		return this.searchService.search(q);
+	}
+	
 }
