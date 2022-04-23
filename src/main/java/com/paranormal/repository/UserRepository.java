116667286797728c9395faae6ca49b9718a26b3f
tahafzl123 <taha.1129@outlook.com>
2022-04-23 03:24:52 +0300

    added findAllByUsername query

--- src/main/java/com/paranormal/repository/UserRepository.java
+++ src/main/java/com/paranormal/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.paranormal.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.paranormal.entity.user.User;
@@ -13,5 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	Boolean existsByEmail(String email);
 
 	Boolean existsByEmailOrUsername(String email, String username);
+
+	@Query("SELECT u FROM User u WHERE u.username like lower(concat('%', :q ,'%'))")
+	Page<User> findAllByUsernameLike(String q, Pageable pageable);
 
 }
