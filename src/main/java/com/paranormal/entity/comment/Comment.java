d509a30dd6ab04155261a6047260a0b9d79d3e36
tahafzl123 <taha.1129@outlook.com>
2022-05-03 15:11:25 +0300

    Removed 'Post' property and '@Table(name:String)' variable to Comments

--- src/main/java/com/paranormal/entity/comment/Comment.java
+++ src/main/java/com/paranormal/entity/comment/Comment.java
@@ -1,18 +1,12 @@
 package com.paranormal.entity.comment;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.paranormal.entity.BaseEntity;
 import com.paranormal.entity.post.Post;
 import com.paranormal.entity.user.User;
@@ -25,7 +19,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "comment")
+@Table(name = "comments")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -44,5 +38,4 @@ public class Comment extends BaseEntity{
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id")
 	private Post post;
-	
 }
