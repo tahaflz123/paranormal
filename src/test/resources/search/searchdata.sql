907a31b3423732f628224620795fd809766c82d5
tahafzl123 <taha.1129@outlook.com>
2022-05-06 15:35:49 +0300

    created new datas for SearchControllerIT

---
+++ src/test/resources/search/searchdata.sql
@@ -0,0 +1,44 @@
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (1, 'crazy_boy', 'daaasdf', '112312312', 'USER', false);
+
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (2, 'goth_girl', 'aaasdfdd', '1232312312', 'USER', false);
+
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (3, 'indeed', 'aaasdfaaa', '1112312312', 'USER', false);
+
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (4, 'rat_boy', 'aaasdffff', '12312312', 'USER', false);
+
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (5, 'username', 'aaaqqqsdf', '12312312', 'USER', false);
+
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (6, 'aaaaa', 'aaasddddf', '12312312', 'USER', false);
+
+INSERT INTO users (id, username, email, password,role, deleted)
+VALUES (7, 'asdfjasdf', 'aaasdfaa@', '12312312', 'USER', false);
+
+INSERT INTO posts (id, header, user_id, deleted)
+VALUES (8, 'the Graveyard', 1, false);
+
+INSERT INTO posts (id, header, user_id,deleted)
+VALUES (9, 'about Chernobyl Disaster', 1,false);
+
+INSERT INTO posts (id, header, user_id,deleted)
+VALUES (10, 'the Elves', 1,false);
+
+INSERT INTO posts (id, header, user_id, deleted)
+VALUES (11, 'my crazy dog', 1, false);
+
+INSERT INTO posts (id, header, user_id,deleted)
+VALUES (12, 'legends for league', 1, false);
+
+INSERT INTO posts (id, header, user_id, deleted)
+VALUES (13, 'ad', 1, false);
+
+INSERT INTO posts (id, header, user_id, deleted)
+VALUES (14, 'am i weird', 1, false);
+
+INSERT INTO posts (id, header, user_id, deleted)
+VALUES (15, 'something', 1, false);
