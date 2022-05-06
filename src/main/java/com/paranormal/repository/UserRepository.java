54f7ad6df9305c98f68591dd82bb6927f3a98c76
tahafzl123 <taha.1129@outlook.com>
2022-05-06 15:48:25 +0300

    'findByEmail' return type wrapped with 'Optional'

--- src/main/java/com/paranormal/repository/UserRepository.java
+++ src/main/java/com/paranormal/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.paranormal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +13,7 @@ import com.paranormal.entity.user.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	User findByEmail(String email);
+	Optional<User> findByEmail(String email);
 
 	Boolean existsByEmail(String email);
 
