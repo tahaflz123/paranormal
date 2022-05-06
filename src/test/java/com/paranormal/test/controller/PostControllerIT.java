1dcc5085c1cb0dda554ca6c76da0de0a50b1b85f
tahafzl123 <taha.1129@outlook.com>
2022-05-06 17:19:01 +0300

    created tests for delete methods

--- src/test/java/com/paranormal/test/controller/PostControllerIT.java
+++ src/test/java/com/paranormal/test/controller/PostControllerIT.java
@@ -1,9 +1,10 @@
 package com.paranormal.test.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -20,6 +21,8 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import com.paranormal.dto.request.PostCreationRequest;
 import com.paranormal.dto.response.ErrorModel;
 import com.paranormal.dto.response.PostResponse;
+import com.paranormal.service.ErrorMessagesService;
+import com.paranormal.service.ErrorMessagesService.Key;
 import com.paranormal.test.helper.LoginHelper;
 import com.paranormal.test.helper.PostHelper;
 
@@ -29,6 +32,8 @@ public class PostControllerIT {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
+	
+	private final Logger logger = LoggerFactory.getLogger(PostControllerIT.class);
 	
 	@Test
 	@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"/cleanup.sql", "/data.sql"})
@@ -100,6 +105,89 @@ public class PostControllerIT {
 		assertEquals(response.getBody().getStatusCode(), 410);
 		assertEquals(response.getBody().getErrorCode(), "NO_RESOURCE");
 		assertEquals(response.getBody().getMessage(), "The following content is deleted or no longer available");
+	}
+	
+	@Test
+	void given_post_id_when_delete_post_then_it_should_return_200_and_true(){
+		String token = LoginHelper.loginWithDefaultUser(restTemplate);
+		Long postId = 14L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<Boolean> response = this.restTemplate.exchange("/api/post/delete?id=" + postId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<Boolean>() {});
+		
+		logger.info("[USER] - Post Delete Test, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getStatusCodeValue(), 200);
+		assertTrue(response.getBody());
+	}
+	
+	@Test
+	void given_post_id_when_delete_post_without_sender_user_and_email_then_it_should_return_403_and_error_model(){
+		String token = LoginHelper.loginWithDefaultUser(restTemplate);
+		Long postId = 13L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<ErrorModel> response = this.restTemplate.exchange("/api/post/delete?id=" + postId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<ErrorModel>() {});
+		
+		logger.info("[USER] - Post Delete Test but Post already deleted, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getBody().getErrorCode(), Key.FORBIDDEN.name());
+		assertEquals(response.getBody().getStatusCode(), 403);
+		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.FORBIDDEN));
+	}
+	
+	@Test
+	void given_deleted_post_id_when_delete_post_then_it_should_return_409_and_error_model(){
+		String token = LoginHelper.loginWithDefaultUser(restTemplate);
+		Long postId = 15L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<ErrorModel> response = this.restTemplate.exchange("/api/post/delete?id=" + postId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<ErrorModel>() {});
+		
+		logger.info("[USER] - Post Delete Test but Post already deleted, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getBody().getErrorCode(), Key.ALREADY_DELETED.name());
+		assertEquals(response.getBody().getStatusCode(), 409);
+		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.ALREADY_DELETED));
+	}
+	
+	@Test
+	void given_deleted_post_id_when_delete_post_as_admin_then_it_should_return_409_and_error_model(){
+		String token = LoginHelper.loginWithAdmin(restTemplate);
+		Long postId = 15L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<ErrorModel> response = this.restTemplate.exchange("/api/post/delete?id=" + postId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<ErrorModel>() {});
+		
+		logger.info("[ADMIN] - Post Delete Test but Post already deleted, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getBody().getErrorCode(), Key.ALREADY_DELETED.name());
+		assertEquals(response.getBody().getStatusCode(), 409);
+		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.ALREADY_DELETED));
 	}
 	
 }
