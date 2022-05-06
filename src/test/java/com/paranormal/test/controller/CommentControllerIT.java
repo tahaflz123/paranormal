1fbd77a2fb6ea5f5e10a064814c0de3e5456b12d
tahafzl123 <taha.1129@outlook.com>
2022-05-06 15:44:35 +0300

    created new test methods

--- src/test/java/com/paranormal/test/controller/CommentControllerIT.java
+++ src/test/java/com/paranormal/test/controller/CommentControllerIT.java
@@ -2,9 +2,12 @@ package com.paranormal.test.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -25,6 +28,7 @@ import com.paranormal.test.helper.CommentHelper;
 import com.paranormal.test.helper.LoginHelper;
 import com.paranormal.test.helper.UserHelper;
 
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Sql(scripts = { "/cleanup.sql", "/data.sql" })
 public class CommentControllerIT {
@@ -32,6 +36,8 @@ public class CommentControllerIT {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	private final Logger logger = LoggerFactory.getLogger(CommentControllerIT.class);
+	
 	@Test
 	@DisplayName(value = "Comment Creation Test")
 	void given_comment_creation_request_when_comment_create_then_it_should_return_200_and_comment() {
@@ -48,6 +54,8 @@ public class CommentControllerIT {
 				HttpMethod.POST, entity, new ParameterizedTypeReference<CommentResponse>() {
 				});
 
+		logger.info("Comment Creation Test, Http Response -> {}", response.toString());
+		
 		assertNotNull(response);
 		assertNotNull(response.getBody());
 		assertEquals(response.getStatusCodeValue(), 200);
@@ -71,6 +79,8 @@ public class CommentControllerIT {
 				HttpMethod.POST, entity, new ParameterizedTypeReference<ErrorModel>() {
 				});
 
+		logger.info("Creating Comment with Invalid ID Test, Http Response -> {}", response.toString());
+		
 		assertNotNull(response);
 		assertNotNull(response.getBody());
 		assertEquals(response.getBody().getStatusCode(), 410);
@@ -90,6 +100,8 @@ public class CommentControllerIT {
 				HttpMethod.POST, entity, new ParameterizedTypeReference<ErrorModel>() {
 				});
 
+		logger.info("Test for Creating Comment without User, Http Response -> ", response.toString());
+		
 		assertNotNull(response.getBody());
 		assertEquals(response.getBody().getStatusCode(), 401);
 		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.UNAUTHORIZED));
@@ -125,4 +137,107 @@ public class CommentControllerIT {
 		assertEquals(response.getBody().getContent(), CommentHelper.CONTENT);
 		assertEquals(response.getBody().getSender().getUsername(), "crazy_boy");
 	}
+	
+	@Test
+	@DisplayName(value = "Test for Delete Comment")
+	void given_comment_id_when_delete_comment_then_it_should_return_200_and_true() {
+		String token = LoginHelper.loginWithCustomEmail("crazy_boy@gmail.com",restTemplate);
+		Long commentId = 3L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<Boolean> response = this.restTemplate.exchange("/api/comment/delete?id=" + commentId,HttpMethod.DELETE, entity, new ParameterizedTypeReference<Boolean>() {});
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertTrue(response.getBody());
+	}
+	
+	@Test
+	@DisplayName(value = "Test for Delete Comment as Admin")
+	void given_comment_id_and_admin_user_when_delete_comment_then_it_should_return_200_and_true() {
+		String token = LoginHelper.loginWithAdmin(restTemplate);
+		Long commentId = 7L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<Boolean> response = this.restTemplate.exchange("/api/comment/delete?id=" + commentId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<Boolean>() {});
+		
+		logger.info("Comment Delete as Admin Test, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+	}
+	
+	/*@Test
+	@DisplayName(value = "Test for Delete Comment with Admin command but with User Role")
+	void given_default_user_when_delete_comment_as_admin_then_it_should_return_403_and_error_model() {
+		String token = LoginHelper.loginWithDefaultUser(restTemplate);
+		Long commentId = 8L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<ErrorModel> response = this.restTemplate.exchange("/api/comment/delete?id=" + commentId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<ErrorModel>() {});
+		
+		logger.info("[ADMIN] - Comment Delete as Admin Test, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getBody().getErrorCode(), Key.FORBIDDEN.name());
+		assertEquals(response.getBody().getStatusCode(), 403);
+		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.FORBIDDEN));
+	}
+	*/
+	@Test
+	@DisplayName(value = "Test for Delete comment but Comment Already Deleted")
+	void given_comment_id_and_default_user_when_delete_comment_then_it_should_return_409_and_error_model() {
+		String token = LoginHelper.loginWithDefaultUser(restTemplate);
+		Long commentId = 9L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<ErrorModel> response = this.restTemplate.exchange("/api/comment/delete?id=" + commentId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<ErrorModel>() {});
+		
+		logger.info("[USER] - Comment Delete as Admin Test but Comment already deleted, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getBody().getErrorCode(), Key.ALREADY_DELETED.name());
+		assertEquals(response.getBody().getStatusCode(), 409);
+		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.ALREADY_DELETED));
+	}
+	
+	@Test
+	@DisplayName(value = "Test for Delete Comment as Admin But Comment Already Deleted")
+	void given_default_user_when_delete_comment_as_admin_then_it_should_return_409_and_error_model() {
+		String token = LoginHelper.loginWithDefaultUser(restTemplate);
+		Long commentId = 9L;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+		
+		final HttpEntity<String> entity = new HttpEntity<>(headers);
+		final ResponseEntity<ErrorModel> response = this.restTemplate.exchange("/api/comment/delete?id=" + commentId, HttpMethod.DELETE,
+				entity, new ParameterizedTypeReference<ErrorModel>() {});
+		
+		logger.info("[ADMIN] - Comment Delete as Admin Test but Comment already deleted, Http Response {} ", response.toString());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(response.getBody().getErrorCode(), Key.ALREADY_DELETED.name());
+		assertEquals(response.getBody().getStatusCode(), 409);
+		assertEquals(response.getBody().getMessage(), ErrorMessagesService.getMessage(Key.ALREADY_DELETED));
+	}
+	
 }
